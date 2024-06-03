27476c43ae43999921d2940a0504df5e54271e0e
Naimul9 <naimulakib100@gmail.com>
2024-06-03 23:11:08 +0600

    navbar added

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDs6JmZBNYU2tjPDsZ_DJPVpR6cc2hA_dE",
+  authDomain: "blood-bridge-6249e.firebaseapp.com",
+  projectId: "blood-bridge-6249e",
+  storageBucket: "blood-bridge-6249e.appspot.com",
+  messagingSenderId: "150162850401",
+  appId: "1:150162850401:web:b04406d45b5626d6b0a020"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 
 import {RouterProvider,} from "react-router-dom";
 import router from './routes/routes';
+import AuthProvider from './Provider/AuthProvider';
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
